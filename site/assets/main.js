303bc460b5138f5c05f447e3dc6a74b51dc59fdd
Akshay S Dinesh <akshay@learnlearn.in>
2022-02-11 22:21:43 +0530

    refactor: keep state and changes together, introduce map data store

--- site/assets/main.js
+++ site/assets/main.js
@@ -1,80 +1,84 @@
 const L = require("leaflet");
-const osmtogeojson = require("osmtogeojson");
 
 const addIndiaBoundaries = require("./india-boundaries");
+const { KERALA_BOUNDS, MIN_ZOOM } = require("./constants");
 const {
-    KERALA_BOUNDS,
-    MIN_ZOOM,
-    OVERPASS_URL,
-    QUERIES,
-} = require("./constants");
-const { fetchJSONWithUrlSearchParams } = require("./utils");
+    startDownload,
+    expect,
+    available,
+    getLayer,
+    getOverview,
+} = require("map-utils");
 
 const map = L.map("map", {
-    maxBounds: KERALA_BOUNDS,
     minZoom: MIN_ZOOM,
     maxBoundsViscosity: 0.9,
 }).fitBounds(KERALA_BOUNDS);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxBounds: KERALA_BOUNDS,
     attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
 addIndiaBoundaries(map);
 
-var currentQid;
-var currentConfiguration = "Boundaries";
-var currentGeoJson;
-var currentLayer;
-var currentLsg = document.querySelector("#lsgTitle").textContent.trim();
+const state = {
+    qid: document.querySelector("[data-mk-key=qid]").textContent.trim(),
+    feature: "Boundaries",
+    len: document.querySelector("[data-mk-key=len]").textContent.trim(),
+    displayedLayers: [],
+};
+
+const setQid = (qid) => {
+    state.qid = qid;
+    const { len, lml } = getOverview(qid);
+    state.len = len;
+    state.lml = lml;
+    mapChangeRequired();
+    skeletonChangeRequired();
+};
+
+const setFeature = (feature) => {
+    state.feature = feature;
+    configureButton = document.getElementById("configuration");
+    configureButton.textContent = feature;
+    mapChangeRequired();
+};
+
 const downloadButton = document.querySelector("#download");
 downloadButton.addEventListener("click", () => {
-    const string = JSON.stringify(currentGeoJson);
-    const bytes = new TextEncoder().encode(string);
-    const blob = new Blob([bytes], { type: "application/json;charset=utf-8" });
-    const a = document.createElement("a");
-    a.href = URL.createObjectURL(blob);
-    a.download = `${currentLsg}.geojson`;
-    a.click();
+    startDownload(state.qid, state.feature);
 });
 
-const getQuery = (qid, config) => {
-    if (config === "Boundaries")
-        return `[out:json] [timeout:500];
-                 relation[wikidata=${qid}];
-                 out geom;`;
-    return `[out:json][timeout:500];
-            (area["wikidata"="${qid}"];nwr(area)[${QUERIES[config]}];);
-            (._;>;);
-            out geom;`;
+const mapChangeRequired = async () => {
+    await expect(state.qid, state.feature);
+    if (!available(state.qid, state.feature)) return;
+
+    const layer = getLayer(state.qid, state.feature);
+    state.displayedLayers.forEach((oldLayer) => map.removeLayer(oldLayer));
+    state.displayedLayers = [layer];
+    map.addLayer(layer);
+    map.flyTo(layer.getBounds().getCenter(), 12);
 };
 
-const loadNewQid = (qid) => {
-    const query = getQuery(qid, currentConfiguration);
+const changeAll = (selector, content) =>
+    document
+        .querySelectorAll(selector)
+        .forEach((el) => (el.textContent = content));
 
-    fetchJSONWithUrlSearchParams(OVERPASS_URL, { data: query })
-        .then((data) => {
-            const geojson = osmtogeojson(data);
-            currentGeoJson = geojson;
-            downloadButton.disabled = false;
-            document.querySelector("#lsgTitle").textContent = `${currentLsg}`;
-            const newlayer = L.geoJSON(geojson, { color: "blue" }).addTo(map);
-            if (currentLayer) map.removeLayer(currentLayer);
-            currentLayer = newlayer;
-            const location = newlayer.getBounds().getCenter();
-            map.flyTo(location, 12);
-            map.setMaxBounds(KERALA_BOUNDS);
-        })
-        .catch((err) => console.error(err));
+const skeletonChangeRequired = () => {
+    changeAll("[data-mk-key=qid]", state.qid);
+    changeAll("[data-mk-key=len]", state.len);
+    changeAll("[data-mk-key=lml]", state.lml);
 };
 
-currentQid = document.querySelector("[data-mk-key=qid]").textContent.trim();
-loadNewQid(currentQid);
+const featureSelectionButtons = document.querySelectorAll("[data-mk-feature]");
+featureSelectionButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        const feature = e.target.getAttribute("data-mk-feature");
+        setFeature(feature);
+    });
+});
 
-const reconfigure = (selection) => {
-    configureButton = document.getElementById("configuration");
-    configureButton.textContent = selection;
-    currentConfiguration = selection;
-    loadNewQid(currentQid);
-};
+mapChangeRequired();
---
+++ site/assets/map-utils.js
@@ -0,0 +1,85 @@
+const osmtogeojson = require("osmtogeojson");
+const L = require("leaflet");
+const { QUERIES } = require("constants");
+const { OVERPASS_URL } = require("constants");
+const { fetchJSONWithUrlSearchParams } = require("utils");
+
+const mapDataStore = {};
+
+const getQuery = (qid, feature) => {
+    if (feature === "Boundaries")
+        return `[out:json] [timeout:500];
+                 relation[wikidata=${qid}];
+                 out geom;`;
+    return `[out:json][timeout:500];
+            (area["wikidata"="${qid}"];nwr(area)[${QUERIES[feature]}];);
+            (._;>;);
+            out geom;`;
+};
+
+const fetchAndStore = async (qid, feature) => {
+    const query = getQuery(qid, feature);
+
+    return fetchJSONWithUrlSearchParams(OVERPASS_URL, { data: query }).then(
+        (data) => {
+            const geojson = osmtogeojson(data);
+            const mapLayer = L.geoJSON(geojson, { color: "blue" });
+            const location = mapLayer.getBounds().getCenter();
+            mapDataStore[`${qid}#${feature}`] = {
+                geojson,
+                mapLayer,
+                location,
+            };
+        }
+    );
+};
+
+const expect = async (qid, feature) => {
+    if (available(qid, feature)) {
+        return true;
+    } else {
+        return fetchAndStore(qid, feature);
+    }
+};
+
+const available = (qid, feature) =>
+    mapDataStore.hasOwnProperty(`${qid}#${feature}`);
+
+const expectSearch = async () => {
+    if (mapDataStore.hasOwnProperty("overview")) return;
+    return fetch("/data.json")
+        .then((res) => res.json())
+        .then((data) => (mapDataStore.overview = data))
+        .then(() => createOverview());
+};
+
+const createOverview = () => {
+    for (const district in Object.keys(mapDataStore.overview)) {
+        for (const el in mapDataStore.overview[district]) {
+            mapDataStore.overview[el.qid] = { ...el, district };
+        }
+    }
+};
+
+const getLayer = (qid, feature) => mapDataStore[`${qid}#${feature}`].mapLayer;
+const getGeojson = (qid, feature) => mapDataStore[`${qid}#${feature}`].geojson;
+
+const startDownload = (qid, feature) => {
+    const string = JSON.stringify(getGeojson(qid, feature));
+    const bytes = new TextEncoder().encode(string);
+    const blob = new Blob([bytes], { type: "application/json;charset=utf-8" });
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = `${qid} - ${feature}.geojson`;
+    a.click();
+};
+
+const getOverview = (qid) => mapDataStore.overview[qid];
+
+module.exports = {
+    expect,
+    available,
+    getLayer,
+    startDownload,
+    getOverview,
+};
